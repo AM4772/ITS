c3e087e033f9ebcddea1c6b9cdee57c69d2f8217
AM4772 <moro_bramanti@hotmail.com>
2023-06-08 19:13:31 -0300

    changes to db.js sequelize instance

--- api/config/db.js
+++ api/config/db.js
@@ -16,22 +16,23 @@ let db =
         database: DB_NAME,
         dialect: "postgres",
         host: DB_HOST,
-        // port: 5432,
+        port: 5432,
         username: DB_USER,
         password: DB_PASSWORD,
         pool: {
-          max: 3,
-          min: 1,
+          max: 5,
+          min: 0,
+          acquire: 30000,
           idle: 10000,
         },
-        dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-          keepAlive: true,
-        },
-        ssl: true,
+        // dialectOptions: {
+        //   ssl: {
+        //     require: true,
+        //     rejectUnauthorized: false,
+        //   },
+        //   keepAlive: true,
+        // },
+        // ssl: true,
       })
     : new Sequelize(
         `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,

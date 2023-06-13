a3fc7fed30819845482bc06f6cf6f1d5f32698a4
AM4772 <moro_bramanti@hotmail.com>
2023-06-13 12:37:53 -0300

    delete sql files from repo

--- api/config/tickets.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO tickets
-VALUES
-(1, 'App crash', 'ksdjflajsdlj{asdj', '1 2 3', 'v 2.0', 1, 'Critical', 'Functional', TRUE, NULL, 'Aldo Moro', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(2, 'App load', 'kjrinndklasd', '1 2 3', 'v 3.0', 1, 'Major', 'Functional', FALSE, NULL, 'Tim Nolan', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(3, 'Log in failed', 'ksdjflajsdlj{asdj', '1 2 3', 'v 2.0', 1, 'Critical', 'Functional', TRUE, NULL, 'Aldo Moro', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(4, 'Menu not visible', 'ksdjflajsdlj{asdj', '1 2 3', 'v 2.0', 1, 'Critical', 'Functional', FALSE, NULL, 'Kim Bassinger', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
--- api/config/users.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO tickets
-VALUES
-(1, 'Mary', 'Reyes', 'mreyes', 'mreyes@test.com', '$2a$10$9.WaCJWYJ.FA43wWqUSX1OCgYiOHYfNwwMg3Fd0Hi.dcDRcWmTLg.', 'Developer', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(2, 'Joe', 'Black', 'jblack', 'jblack@test.com', '$2a$10$9.WaCJWYJ.FA43wWqUSX1OCgYiOHYfNwwMg3Fd0Hi.dcDRcWmTLg.', 'Customer', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(3, 'Rita', 'Hayworth', 'rhayworth', 'rhayworth@test.com', '$2a$10$9.WaCJWYJ.FA43wWqUSX1OCgYiOHYfNwwMg3Fd0Hi.dcDRcWmTLg.', 'Developer', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(4, 'Tim', 'Burton', 'tburton', 'tburton@test.com', '$2a$10$9.WaCJWYJ.FA43wWqUSX1OCgYiOHYfNwwMg3Fd0Hi.dcDRcWmTLg.', 'Admin', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(5, 'Eli', 'Wasp', 'ewasp', 'ewasp@test.com', '$2a$10$9.WaCJWYJ.FA43wWqUSX1OCgYiOHYfNwwMg3Fd0Hi.dcDRcWmTLg.', 'Manager', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
